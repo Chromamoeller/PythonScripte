093a975a407593ee57a5bc640d5cf13b211169a0
Chromamoeller <der.chris.moeller@gmail.com>
2023-08-28 09:05:15 +0200

    Durchsucht einen Ordner sowie alle unterordner nach Bild datein und kopiert diese in neue ordner sortiert nach erstellungsJahr

--- SearchPicturesAndSortByName.py
+++ SearchPicturesAndSortByName.py
@@ -1,18 +1,22 @@
 import os
 import shutil
+from datetime import datetime
 
 # Funktion, um Bild-Dateierweiterungen zu überprüfen
 def is_image_file(filename):
     image_extensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp']
     return any(filename.lower().endswith(ext) for ext in image_extensions)
 
-# Funktion, um Bilder in neuen Ordnern zu organisieren
-def organize_image_files(src_folder, dest_folder):
+# Funktion, um Bilder nach Erstellungsdatum zu organisieren
+def organize_image_files_by_creation_date(src_folder, dest_folder):
     for root, dirs, files in os.walk(src_folder):
         for file in files:
             if is_image_file(file):
                 src_path = os.path.join(root, file)
-                dest_subfolder = os.path.join(dest_folder, file[0].upper())
+                creation_time = os.path.getctime(src_path)
+                creation_date = datetime.fromtimestamp(creation_time)
+                year = str(creation_date.year)
+                dest_subfolder = os.path.join(dest_folder, year)
                 os.makedirs(dest_subfolder, exist_ok=True)
                 dest_path = os.path.join(dest_subfolder, file)
                 shutil.move(src_path, dest_path)
@@ -23,8 +27,8 @@ def main():
     source_folder = input("Bitte gib den Pfad zum Quellordner ein: ")
     destination_folder = input("Bitte gib den Pfad zum Zielordner ein: ")
 
-    organize_image_files(source_folder, destination_folder)
-    print("Bilddateien wurden alphabetisch in neue Ordner verschoben.")
+    organize_image_files_by_creation_date(source_folder, destination_folder)
+    print("Bilddateien wurden nach Erstellungsdatum in neue Ordner verschoben.")
 
 if __name__ == "__main__":
     main()
